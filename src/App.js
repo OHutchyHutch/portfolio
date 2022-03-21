90b6784b8631856177ded7815c4898120aeed3e0
OHutchyHutch <ohutchins1@student.gsu.edu>
2022-03-21 18:07:06 -0400

    Added Tailwind

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Site is currently being remodeled with React and Tailwind. Hope to see you soon!</h2>
+        <h2 className="font-bold text-sky-300">Site is currently being remodeled with React and Tailwind. Hope to see you soon!</h2>
       </header>
     </div>
   );
